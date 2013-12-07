4bd1fc304ae97cf88fe0f0aed9d4330745dd788c
Vincent Viallet <vincent@devo.ps>
2013-12-07 16:16:50 +0800

    Add angular.

---
+++ js/vogueultra.js
@@ -0,0 +1,71 @@
+angular.module('vogueultra', ['ngRoute'])
+
+.config(function($route) {
+  $route
+    .when('/', {
+      controller: 'HomeCtrl',
+      templateUrl: 'home.html'
+    })
+    .when('/collection/:collectionId', {
+      controller: 'CollectionCtrl',
+      templateUrl: 'collection.html'
+    })
+    .when('/article/:articleId', {
+      controller: 'ArticleCtrl',
+      templateUrl: 'Article.html'
+    })
+    .when('/blog', {
+      controller: 'BlogCtrl',
+      templateUrl: 'blog.html'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+
+.controller('HomeCtrl', function($scope, $location, $routeParams) {
+
+})
+
+.controller('CollectionCtrl', function($scope, $location, $routeParams) {
+  if ($routeParams.collectionId == 'vogueultra') {
+    $scope.articles = [
+      {id: 1, name: 'article 1', description: 'Awesome article in collection vogueultra'},
+      {id: 2, name: 'article 2', description: 'Awesome article in collection vogueultra'},
+    ]
+  }
+  else if ($routeParams.collectionId == 'designers') {
+    $scope.articles = [
+      {id: 3, name: 'article 3', description: 'Awesome article in collection designers'},
+      {id: 4, name: 'article 4', description: 'Awesome article in collection designers'},
+    ]
+  }
+})
+
+.controller('ArticleCtrl', function($scope, $location, $routeParams) {
+  if ($routeParams.ArticleId == '1') {
+    $scope.article = {
+      id: 1, 
+      name: 'article 1', 
+      description: 'Awesome article in collection 1'
+    }
+  }
+  else if ($routeParams.ArticleId == '2') {
+    $scope.article = {
+      id: 2, 
+      name: 'article 2', 
+      description: 'Awesome article in collection 1'
+    }
+  }
+  else {
+    $scope.article = {
+      id: 3, 
+      name: 'article 3', 
+      description: 'catchall'
+    }
+  }
+})
+
+.controller('BlogCtrl', function($scope, $location, $routeParams) {
+  
+})
